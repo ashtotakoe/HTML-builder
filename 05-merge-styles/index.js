94dcc1018ea9cd6f765c878738bf1e9dd1aeecc2
Егор Мартынов <egormartynov12@gmail.com>
2023-05-08 07:22:29 +0300

    feat: solve task 5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const { readdir, readFile, writeFile } = require('fs');
+
+const bundle = path.join(__dirname, 'project-dist', 'bundle.css');
+const targetDir = path.join(__dirname, 'styles');
+let str = '';
+
+readdir(targetDir, { withFileTypes: true }, (err, styles) => {
+  if (err) throw err;
+  styles.forEach((style) => {
+    readFile(path.join(targetDir, style.name), (err, buffer) => {
+      if (err) throw err;
+      str += buffer.toString();
+      writeFile(bundle, str, (err) => {
+        if (err) throw err;
+      });
+    });
+  });
+});
