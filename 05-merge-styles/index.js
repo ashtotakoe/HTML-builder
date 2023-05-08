c95f553c6d4ce67a9867e996c7c953095cb8dd42
Егор Мартынов <egormartynov12@gmail.com>
2023-05-08 07:47:33 +0300

    fix: add file checker in  task 5

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,12 +5,17 @@ const bundle = path.join(__dirname, 'project-dist', 'bundle.css');
 const targetDir = path.join(__dirname, 'styles');
 let str = '';
 
-readdir(targetDir, { withFileTypes: true }, (err, styles) => {
+readdir(targetDir, { withFileTypes: true }, (err, arr) => {
   if (err) throw err;
+  const styles = arr.filter(
+    (style) => path.extname(style.name) === '.css' && style.isFile()
+  );
+
   styles.forEach((style) => {
     readFile(path.join(targetDir, style.name), (err, buffer) => {
       if (err) throw err;
       str += buffer.toString();
+
       writeFile(bundle, str, (err) => {
         if (err) throw err;
       });
